f2899e6369f8d39fa8b3dce29cdb6909473dc6e9
Batyr Atayev <atayev2012@gmail.com>
2024-06-21 00:18:50 +0300

    Registered series router

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
-from user_app.router import router as user_app_router
+
 from db_app.database import SessionLocal, engine, Base
 # from db_app.models import User, UserAccessLevel, Series, MySeries, SeriesRating, SeriesRatingUser
+from series_app.router import router as series_app_router
+from user_app.router import router as user_app_router
 
-
-# from series_app.router import router as series_app_router
 # create database
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(user_app_router)
-# app.include_router(series_app_router)
+app.include_router(series_app_router)
 
 
 @app.get("/")
