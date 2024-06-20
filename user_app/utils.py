c64d05b6f3090a9ab38b6589bcf7bf39a5a92806
Batyr Atayev <atayev2012@gmail.com>
2024-06-21 00:20:29 +0300

    str_to_uuid function now handles HTTP exceptions

--- user_app/utils.py
+++ user_app/utils.py
@@ -1,15 +1,45 @@
 from typing import Annotated, Union
 from sqlalchemy.orm import Session
 from sqlalchemy import func
-# from db_app.models import UserAccessLevel, User, Series, MySeries
+from db_app.models import User
 # from user_app.schemas import UserFound, MySeriesForUserFound
 from fastapi import HTTPException, status
 from bcrypt import hashpw, gensalt, checkpw
 from uuid import UUID
+from db_app.database import Base
 
 
+# convert user id str from database to UUID variable
 def str_to_uuid(uuid_code: str):
-    return UUID(hex=uuid_code, version=4)
+    try:
+        user_uuid = UUID(hex=uuid_code, version=4)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={
+                "status": "fail",
+                "message": "User-Id ERROR: " + e.args[0],
+                "data": None
+            }
+        )
+    return user_uuid
+
+
+# verify that user exists
+def verify_item_exists(item_uuid: UUID, item_class: Base, msg_name: str, session: Session) -> Base | None:
+    user = session.get(item_class, item_uuid)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=dict(status="fail",
+                        message=f"{msg_name} "
+                                f"Error: no {msg_name.lower()} "
+                                f"with provided id was found",
+                        data=None)
+        )
+
+    return user
 
 
 def hash_password(password: str) -> str:
